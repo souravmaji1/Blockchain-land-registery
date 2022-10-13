3373f6e5e8132953808780ca58452eaee05cefb8
sourav <majisourav255@gmail.com>
2022-10-13 19:00:57 +0530

    first commit

---
+++ lib/constant/constants.dart
@@ -0,0 +1,13 @@
+String mapBoxApiKey =
+    'pk.eyJ1Ijoic2F1cmFiaG13IiwiYSI6ImNreTRiYzNidjBhMTkydnB2dmpoeGt4ZmgifQ.2QZ4CsNiygDTAhkqASpbPg';
+
+String nftStorageApiKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDlhZjY0MWI3QkZiNWY0RjBDZmJBNjkyRTJlREVkNjUzYmMzMDNERDkiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY1NzIyMzkzNjk0OCwibmFtZSI6Im5ldyBuZnQifQ.yhdXXpsnzMesxcvJ6kXeEoVZF7nVBHQM7PsxDnBb8nc';
+
+const String rpcUrl =
+    "https://rpc-mumbai.matic.today";
+//"http://127.0.0.1:7545";
+
+const String contractAddress = "0xffBFCf4CE7a1050925fFaa5f0730fa541Ee60dAf";
+
+const int chainId = 80001; //Ropsten-3, Polygon testnet-80001, local ganache-1337
